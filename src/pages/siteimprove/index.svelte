093242b224fd6bdad4db2a47d3a254b385fb8c9e
Gorm <gormroedder@gmail.com>
2020-10-24 00:20:02 +0200

    testing functions

---
+++ src/pages/siteimprove/index.svelte
@@ -0,0 +1,20 @@
+<script>
+
+  async function getInfo () {
+    return await fetch('.netlify/functions/siteimprove', {
+			method: 'POST',
+			body: JSON.stringify({
+				endpoint: "sites"
+			})
+    })
+  }
+
+  let promise = getInfo()
+
+</script>
+
+{#await promise}
+	<p>...waiting</p>
+{:then result}
+	<p>The result is {result}</p>
+{/await}
